468bfebcd02ae1c7bd77b679559729dff10cb1c9
Piotr Dobrzyński <50482622+piotrdobrzynski@users.noreply.github.com>
2022-07-01 14:48:54 +0200

    Add files via upload

---
+++ src/___App.vue
@@ -0,0 +1,25 @@
+<template>
+    <table>
+        <tr v-for="d in data" :key="d.index">
+            <td>{{d.index}}</td>
+            <td>{{d._id}}</td>
+            <td>{{d.guid}}</td>
+        </tr>
+    </table>
+</template>
+
+<script>
+    import Data from './data.json'
+    export default {
+        name: 'App',
+        data() {
+            return {
+                data: Data
+            };
+        }
+    }
+</script>
+
+<style>
+  @import './Own.css';
+</style>
